296c8ad2f4ba58ce42993ad6f54f3e68237fba35
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-04-24 17:30:46 -0400

    got rhombus grid working

--- src/App.tsx
+++ src/App.tsx
@@ -32,19 +32,14 @@ export default class App extends React.Component {
   render() {
     return (
       /** */
-      <div className="flex h-lvh w-lvw text-white opacity-90 bg-gradient-to-tr from-zinc-900 via-purple-700 to-sky-500">
-        <div className="backdrop-blur-lg m-auto font-0">
+      <div className="main flex h-lvh w-lvw text-white opacity-90 bg-gradient-to-tr from-zinc-900 via-purple-700 to-sky-500">
+        <div className="rhombox backdrop-blur-lg">
           {mixes.map((mix: Mix, index: Number) => <>
-            <div id="rhombus" className="bg-white w-32 m-4 h-32 -mb-16 inline-block [clip-path:polygon(50% 0, 100% 50%, 50% 100%, 0 50%)]">
+            <div  >
+              OO1
             </div>
-
-            <div id="rhombus" className="bg-white w-32 m-4 h-32 -mb-16 inline-block [clip-path:polygon(50% 0, 100% 50%, 50% 100%, 0 50%)]">
-            </div>
-
-            <div id="rhombus" className="bg-white w-32 m-4 h-32 -mb-16 inline-block [clip-path:polygon(50% 0, 100% 50%, 50% 100%, 0 50%)]">
-            </div>
-
-            <div id="rhombus" className="bg-white w-32 m-4 h-32 -mb-16 inline-block [clip-path:polygon(50% 0, 100% 50%, 50% 100%, 0 50%)]">
+            <div  >
+              Testie
             </div>
           </>
           )}
@@ -55,21 +50,22 @@ export default class App extends React.Component {
 
           {/*    <GalaxyMap stars={40}>
             <div className="flex flex-row flex-wrap">
-              {mixes.map((mix: Mix, index: number) => {
-                return (
-                  <SpaceSector
-                    key={`${index}-sector`}
-                    mixData={mix.data}
-                    onClick={() => this.playMix(index)}
-                    selected={index === this.selectedMixIdx}
-                    index={index}
-                  />
-                );
-              })}
-            </div>
-          </GalaxyMap> */}
-        </div>
+        {mixes.map((mix: Mix, index: number) => {
+          return (
+            <SpaceSector
+              key={`${index}-sector`}
+              mixData={mix.data}
+              onClick={() => this.playMix(index)}
+              selected={index === this.selectedMixIdx}
+              index={index}
+            />
+          );
+        })}
       </div>
+          </GalaxyMap > */
+          }
+        </div>
+      </div >
     );
   }
 }
