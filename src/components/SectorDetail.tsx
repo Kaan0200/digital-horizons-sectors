4c9799e80ec8c9df139be154a388e7fe74ed8bf3
A. Kaan Atesoglu <aka43@case.edu>
2025-09-10 18:25:48 -0400

    caught up changes, added husky

--- src/components/SectorDetail.tsx
+++ src/components/SectorDetail.tsx
@@ -9,16 +9,17 @@ export default function SectorDetail(props: SectorDetailProps): React.JSX.Elemen
     let nav = useNavigate();
     return (
         <div
-            className="w-full h-full absolute content-center"
+            className="flex content-center absolute"
             onClick={() => {
                 nav("..");
             }}
         >
             <div
                 onClick={(e) => { e.stopPropagation();}}
-                className="w-80 h-80 bg-amber-600 m-auto"
+                className="w-80 h-80 bg-amber-600 absolute"
             >
                 This screen shows the details of the mix playing
+                <button>Play Me</button>
             </div>
         </div>
     )
