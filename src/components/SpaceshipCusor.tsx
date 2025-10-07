b80766a649ebe544168a2cae7b801f40461eb787
A. Kaan Atesoglu <aka43@case.edu>
2025-10-07 01:11:23 -0400

    aligned code organization between both animating components

--- src/components/SpaceshipCusor.tsx
+++ src/components/SpaceshipCusor.tsx
@@ -6,6 +6,28 @@ interface FollowCursorProps {
   color?: string;
 }
 
+class Dot {
+  position: { x: number; y: number };
+  width: number;
+  lag: number;
+
+  constructor(x: number, y: number, width: number, lag: number) {
+    this.position = { x, y };
+    this.width = width;
+    this.lag = lag;
+  }
+
+  moveTowards(x: number, y: number, color: string, context: CanvasRenderingContext2D) {
+    this.position.x += (x - this.position.x) / this.lag;
+    this.position.y += (y - this.position.y) / this.lag;
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(this.position.x, this.position.y, this.width, 0, 2 * Math.PI);
+    context.fill();
+    context.closePath();
+  }
+}
+
 /**
  * A Cursor following component
  *
@@ -21,28 +43,6 @@ const SpaceshipCursor: React.FC<FollowCursorProps> = ({ color = '#EAE3CA' }) => 
     let height = window.innerHeight;
     const cursor = { x: width / 2, y: height / 2 };
     const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
-
-    class Dot {
-      position: { x: number; y: number };
-      width: number;
-      lag: number;
-
-      constructor(x: number, y: number, width: number, lag: number) {
-        this.position = { x, y };
-        this.width = width;
-        this.lag = lag;
-      }
-
-      moveTowards(x: number, y: number, context: CanvasRenderingContext2D) {
-        this.position.x += (x - this.position.x) / this.lag;
-        this.position.y += (y - this.position.y) / this.lag;
-        context.fillStyle = color;
-        context.beginPath();
-        context.arc(this.position.x, this.position.y, this.width, 0, 2 * Math.PI);
-        context.fill();
-        context.closePath();
-      }
-    }
 
     const dot = new Dot(width / 2, height / 2, 10, 10);
 
@@ -63,7 +63,7 @@ const SpaceshipCursor: React.FC<FollowCursorProps> = ({ color = '#EAE3CA' }) => 
     const updateDot = () => {
       if (context) {
         context.clearRect(0, 0, width, height);
-        dot.moveTowards(cursor.x, cursor.y, context);
+        dot.moveTowards(cursor.x, cursor.y, color, context);
       }
     };
 
@@ -94,7 +94,9 @@ const SpaceshipCursor: React.FC<FollowCursorProps> = ({ color = '#EAE3CA' }) => 
     };
 
     const destroy = () => {
-      if (canvas) canvas.remove();
+      if (canvas) {
+        canvas.remove();
+      }
       cancelAnimationFrame(animationFrame);
       window.removeEventListener('mousemove', onMouseMove);
       window.removeEventListener('resize', onWindowResize);
--- src/components/Starfield.tsx
+++ src/components/Starfield.tsx
@@ -41,143 +41,151 @@ export const Starfield = observer((props: Props) => {
 
   useEffect(() => {
     const canvas = document.getElementById('starfield') as HTMLCanvasElement;
+    let width: number, height: number;
 
-    if (canvas) {
-      const c = canvas.getContext('2d');
-
-      if (c) {
-        let w = window.innerWidth;
-        let h = window.innerHeight;
-
-        const setCanvasExtents = () => {
-          canvas.width = w;
-          canvas.height = h;
+    const makeStars = (count: number): { x: number; y: number; z: number }[] => {
+      const out = [];
+      for (let i = 0; i < count; i++) {
+        const s = {
+          x: Math.random() * 1600 - 800,
+          y: Math.random() * 900 - 450,
+          z: Math.random() * 1000,
         };
+        out.push(s);
+      }
+      return out;
+    };
+
+    const stars = makeStars(starCount);
+
+    let colorCounter: number = 0;
+    const clear = () => {
+      const c = canvas.getContext('2d');
+      if (c) {
+        c.fillStyle = backgroundColor;
+        c.fillRect(0, 0, canvas.width, canvas.height);
+      }
+    };
+
+    const putPixel = (x: number, y: number, brightness: number) => {
+      const rezColor = generateRGB(colorCounter++, 2000000);
+
+      const rgb =
+        'rgba(' +
+        rezColor[0] +
+        ',' +
+        rezColor[1] +
+        ',' +
+        rezColor[2] +
+        ',' +
+        brightness +
+        ')';
+      const c = canvas.getContext('2d');
+      if (c) {
+        c.fillStyle = rgb;
+        c.fillRect(x, y, 4, 4);
+      }
+    };
+
+    const generateRGB = (
+      n: number,
+      maximum: number,
+    ): [r: number, g: number, b: number] => {
+      const a = (15.7079 * n) / (3 * maximum) + 1.5708;
+      const r = Math.sin(a) * 192 + 128;
+      const g = Math.sin(a - 2.0944) * 192 + 128;
+      const b = Math.sin(a - 4.1888) * 192 + 128;
+
+      return [r, g, b];
+    };
+
+    const moveStars = (distance: number) => {
+      const count = stars.length;
+      for (let i = 0; i < count; i++) {
+        const s = stars[i];
+        s.z -= distance;
+        while (s.z <= 1) {
+          s.z += 1000;
+        }
+      }
+    };
+
+    let prevTime: number;
+    const init = (time: number) => {
+      prevTime = time;
+      requestAnimationFrame(tick);
+    };
+
+    // ^^^^ END OF SETUP ^^^^
+
+    // Animation looping function, asks to repeat every time the engine is ready to animate
+    const tick = (time: number) => {
+      const elapsed = time - prevTime;
+      prevTime = time;
+
+      moveStars(elapsed * (store.isPlaying ? 0.1 : speedFactor));
+
+      clear();
+
+      const cx = width / 2;
+      const cy = height / 2;
+
+      const count = stars.length;
+      for (let i = 0; i < count; i++) {
+        const star = stars[i];
+
+        const x = cx + star.x / (star.z * 0.001);
+        const y = cy + star.y / (star.z * 0.001);
+
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+          continue;
+        }
+
+        const d = star.z / 1000.0;
+        const b = 1 - d * d;
+
+        putPixel(x, y, b);
+      }
+
+      requestAnimationFrame(tick);
+    };
+
+    const destroy = () => {
+      window.onresize = null;
+    };
+
+    const start = () => {
+      requestAnimationFrame(init);
+
+      // add window resize listener:
+      window.addEventListener('resize', function () {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        setCanvasExtents();
+      });
+      const setCanvasExtents = () => {
+        canvas.width = width;
+        canvas.height = height;
+      };
+
+      if (canvas) {
+        width = window.innerWidth;
+        height = window.innerHeight;
 
         setCanvasExtents();
 
         window.onresize = () => {
           setCanvasExtents();
         };
-
-        const makeStars = (count: number): { x: number; y: number; z: number }[] => {
-          const out = [];
-          for (let i = 0; i < count; i++) {
-            const s = {
-              x: Math.random() * 1600 - 800,
-              y: Math.random() * 900 - 450,
-              z: Math.random() * 1000,
-            };
-            out.push(s);
-          }
-          return out;
-        };
-
-        const stars = makeStars(starCount);
-
-        let colorCounter: number = 0;
-
-        const clear = () => {
-          c.fillStyle = backgroundColor;
-          c.fillRect(0, 0, canvas.width, canvas.height);
-        };
-
-        const putPixel = (x: number, y: number, brightness: number) => {
-          const rezColor = generateRGB(colorCounter++, 2000000);
-
-          const rgb =
-            'rgba(' +
-            rezColor[0] +
-            ',' +
-            rezColor[1] +
-            ',' +
-            rezColor[2] +
-            ',' +
-            brightness +
-            ')';
-          c.fillStyle = rgb;
-          c.fillRect(x, y, 4, 4);
-        };
-
-        const generateRGB = (
-          n: number,
-          maximum: number,
-        ): [r: number, g: number, b: number] => {
-          const a = (15.7079 * n) / (3 * maximum) + 1.5708;
-          const r = Math.sin(a) * 192 + 128;
-          const g = Math.sin(a - 2.0944) * 192 + 128;
-          const b = Math.sin(a - 4.1888) * 192 + 128;
-
-          return [r, g, b];
-        };
-
-        const moveStars = (distance: number) => {
-          const count = stars.length;
-          for (let i = 0; i < count; i++) {
-            const s = stars[i];
-            s.z -= distance;
-            while (s.z <= 1) {
-              s.z += 1000;
-            }
-          }
-        };
-
-        let prevTime: number;
-        const init = (time: number) => {
-          prevTime = time;
-          requestAnimationFrame(tick);
-        };
-        // ^^^^ END OF SETUP ^^^^
-
-        // Animation looping function, asks to repeat every time the engine is ready to animate
-        const tick = (time: number) => {
-          const elapsed = time - prevTime;
-          prevTime = time;
-
-          moveStars(elapsed * (store.isPlaying ? 0.1 : speedFactor));
-
-          clear();
-
-          const cx = w / 2;
-          const cy = h / 2;
-
-          const count = stars.length;
-          for (let i = 0; i < count; i++) {
-            const star = stars[i];
-
-            const x = cx + star.x / (star.z * 0.001);
-            const y = cy + star.y / (star.z * 0.001);
-
-            if (x < 0 || x >= w || y < 0 || y >= h) {
-              continue;
-            }
-
-            const d = star.z / 1000.0;
-            const b = 1 - d * d;
-
-            putPixel(x, y, b);
-          }
-
-          requestAnimationFrame(tick);
-        };
-
-        requestAnimationFrame(init);
-
-        // add window resize listener:
-        window.addEventListener('resize', function () {
-          w = window.innerWidth;
-          h = window.innerHeight;
-          setCanvasExtents();
-        });
       } else {
         console.error('Could not get 2d context from canvas element');
       }
-    } else {
-      console.error('Could not find canvas element with id "starfield"');
-    }
+    };
+
+    start();
 
     return () => {
-      window.onresize = null;
+      destroy();
     };
   }, [restColor, backgroundColor, speedFactor, starCount]);
 
