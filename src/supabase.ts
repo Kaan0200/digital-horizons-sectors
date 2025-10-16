0d3c20480763e4738fc75b76e62bde1cfdc9f0f1
A. Kaan Atesoglu <aka43@case.edu>
2025-10-15 22:29:45 -0400

    changed sector color, added vite vs node env access

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+// DEV
+let supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
+let supabaseKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+
+// PROD
+if (!supabaseUrl) {
+    supabaseUrl = process.env.VITE_APP_SUPABASE_URL;
+}
+if (!supabaseKey) {
+    supabaseKey = process.env.VITE_APP_SUPABASE_ANON_KEY;
+}
 
 const supabase = createClient(supabaseUrl || '', supabaseKey || '');
 
